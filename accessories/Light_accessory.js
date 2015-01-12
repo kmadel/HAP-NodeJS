cd05f20df71c5880b9b5ba843dc7e9131785011a
kmadel <kmadel@mac.com>
2015-01-12 11:18:02 -0500

    Update Light_accessory.js

--- accessories/Light_accessory.js
+++ accessories/Light_accessory.js
@@ -5,7 +5,8 @@ var exports = module.exports = {};
 var execute = function(accessory,characteristic,value){ 
 	
 	var http = require('http');
-	var post_data = 'cmd=DeviceSendCommand&data=%3Cgip%3E%3Cversion%3E1%3C%2Fversion%3E%3Ctoken%3E1234567890%3C%2Ftoken%3E%3Cdid%3E216600238738871650%3C%2Fdid%3E%3Cvalue%3E1%3C%2Fvalue%3E%3C%2Fgip%3E&fmt=xml';
+	var gipValue = value ? 1:0;
+	var post_data = 'cmd=DeviceSendCommand&data=%3Cgip%3E%3Cversion%3E1%3C%2Fversion%3E%3Ctoken%3E1234567890%3C%2Ftoken%3E%3Cdid%3E216600238738871650%3C%2Fdid%3E%3Cvalue%3E' + gipValue + '%3C%2Fvalue%3E%3C%2Fgip%3E&fmt=xml';
 	
 	// An object of options to indicate where to post to
 	var post_options = {
@@ -30,7 +31,7 @@ var execute = function(accessory,characteristic,value){
 	// post the data
 	post_req.write(post_data);
 	post_req.end(); 
-	console.log("executed accessory: " + accessory + ", with value: " +  value + "."); 
+	console.log("executed accessory: " + accessory + ", with value: " +  value + ", with gipValue: " +  gipValue + "."); 
 }
 
 exports.accessory = {
